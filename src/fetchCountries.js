86fbba90eeb34ab556f0c36481d9bc1f00c91deb
taras-kozachok <taras.kozachok.goit@gmail.com>
2022-04-26 17:02:39 +0300

    ver 2.0

---
+++ src/fetchCountries.js
@@ -0,0 +1,10 @@
+export default function fetchCountries(name) {
+  const address = 'https://restcountries.com/v3.1/name/';
+  const options = `?fields=name,capital,population,flags,languages`;
+  return fetch(`${address}${name}${options}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
-import './css/styles.css';
+import fetchCountries from "./fetchCountries";
+import debounce from "lodash.debounce";
+import { Notify } from "notiflix/build/notiflix-notify-aio";
 
-const DEBOUNCE_DELAY = 300;
+const countryInfo = document.querySelector('.country-info');
+const inputBox = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const DELAY_DEBOUNCE = 300;
+
+inputBox.addEventListener('input', debounce(onInputBoxChange, DELAY_DEBOUNCE));
+
+function onInputBoxChange() {
+  const isInput = inputBox.value.trim();
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+  if (isInput) {
+    fetchCountries(isInput).then(dataProc)
+      .catch(error => {
+        Notify.failure("Oops, there is no country with that name");
+        console.log(error);
+      });
+  }
+
+  function dataProc(data) {
+    if (data.length > 10) {
+      Notify.info("Too many matches found. Please enter a more specific name.");
+      return;
+    }
+    markup(data);
+  }
+
+  function markup(data) {
+    const markupData = data.map(({ flags: { svg }, name: { official } }) => {
+      return `<li><img src="${svg}" alt="${official}>" width = "20" height = "10"/>${official}</li>`;
+    })
+      .join('');
+    if (data.length === 1) {
+      const languages = Object.values(data[0].languages).join(', ');
+      const markupInfo = `<ul> <li>Capital: ${data[0].capital}</li> <li>Population: ${data[0].population}</li> <li>Languages: ${languages}</li> </ul>`;
+      countryInfo.insertAdjacentHTML('afterbegin', markupInfo);
+    }
+    return countryList.insertAdjacentHTML('afterbegin', markupData);
+  }
+}
